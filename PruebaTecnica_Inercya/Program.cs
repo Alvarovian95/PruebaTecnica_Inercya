32f229146884e3b804202d6839402f2aad856dcf
AlvaroVian <alvarovian95@gmail.com>
2023-04-19 20:18:18 +0200

    First commit

---
+++ PruebaTecnica_Inercya/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PruebaTecnica_Inercya
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
