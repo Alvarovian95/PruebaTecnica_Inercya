b639257c07b52f91117b47e69e6a822874e2d29d
AlvaroVian <alvarovian95@gmail.com>
2023-04-28 13:09:23 +0200

    Numero Aleatorio

---
+++ Random/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+
+namespace Random
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Random numeroAleatorio = new System.Random();
+            int numTotal = 10000000;
+
+            var rutaArchivo = ConfigurationManager.AppSettings["ArchivoNumeros"];
+            rutaArchivo = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, rutaArchivo);
+          
+            HashSet<int> usedNums = new HashSet<int>();
+        
+            using (StreamWriter writer = new StreamWriter(rutaArchivo))
+            {
+                for (int i = 0; i < numTotal; i++)
+                {
+                    int numero = numeroAleatorio.Next();
+            
+                    while (usedNums.Contains(numero))
+                    {
+                        numero = numeroAleatorio.Next();
+                    }
+                  
+                    usedNums.Add(numero);
+                    writer.WriteLine(numero);
+                }
+            }
+        }
+    }
+}
