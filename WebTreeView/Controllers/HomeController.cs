aa691b1ded10d24c2b535edbe418bab54572078c
AlvaroVian <alvarovian95@gmail.com>
2023-05-03 10:34:10 +0200

    WebTreeView

---
+++ WebTreeView/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Hosting;
+using System.Web.Mvc;
+using WebTreeView.Models;
+
+namespace WebTreeView.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            string path = HostingEnvironment.MapPath("~/App_Data/items.json");
+            string json = System.IO.File.ReadAllText(path);
+            List<ItemModel> items = JsonConvert.DeserializeObject<List<ItemModel>>(json);
+
+            return View(items);
+        }       
+    }
+}
---
+++ WebTreeView/Models/ItemModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebTreeView.Models
+{
+    public class ItemModel
+    {
+        public string Name { get; set; }
+        public List<ItemModel> Children { get; set; }
+    }
+}
